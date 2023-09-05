0a450c07cd997096f41c023165d832228195f783
Isak Herrera <isakasa@stud.ntnu.no>
2023-09-05 10:51:22 +0200

    FEAT: Testing python installation.

--- hello.py
+++ hello.py
@@ -1,3 +1,9 @@
+import numpy as np
+
+arr1 = np.array([[2, -7, 5], [-6, 2, 0]])
+arr2 = np.array([[0, -7, 8], [5, -2, 9], [5, -2, 9]])
+
 #Just checking that python was installed correctly
 print("Hello world!")
 print("I am operative!")
+print(arr1 @ arr2)
