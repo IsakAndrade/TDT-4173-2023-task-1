43598ed155590a2b0a565d0c61c112f5a48b2823
= <=>
2023-10-14 23:48:30 +0200

    FEAT: Entropy

--- decision_tree/decision_tree.py
+++ decision_tree/decision_tree.py
@@ -12,7 +12,7 @@ import pandas as pd
 #   Pass on: 
 # Tests and a threshold that is set by the user of the tree
 class TreeNode:
-    def __init__(self, feature: str = None, threshold: float = None, left: TreeNode = None, right: TreeNode = None, *, value: str = None):
+    def __init__(self, feature: str = None, threshold: float = None, left = None, right = None, *, value: str = None):
         self.feature   = feature    # Index of feature to split on
         self.threshold = threshold  # Threshold value for the split
         self.value     = value      # Prediction value for leaf nodes
@@ -43,72 +43,87 @@ class DecisionTree:
         """
 
         # Transforming the data so that it becomes easier to work with.
-        X_numeric = pd.DataFrame
+        
         
         self.features = X.columns.values
-        
+        self.numeric_features = list(range(len(X.columns.values)))
+
+        X_numeric = X.copy()
+        X_numeric.columns = self.numeric_features
+
+        thresh_mappings = []
         for numeric_feature, feature in enumerate(self.features):
             thresh = X[feature].unique() 
             thresh_numeric = list(range(len(thresh)))
-            string_to_number_mapping = [dict(zip(thresh, number)) for number in thresh_numeric]
-
-            X_numeric[numeric_feature] = X[feature].factorize()
-
-
+            thresh_mapping = [dict(zip(thresh, thresh_numeric)) for num in thresh_numeric][0]
+            thresh_mappings.append(thresh_mapping)
+            
+            data = X[feature]
+            X_numeric[numeric_feature] = pd.Series([thresh_mapping[val] for val in data])
 
 
         self.tree = self._grow_tree(X_numeric, y)
 
-    def _grow_tree(self, X: pd.DataFrame, y: pd.Series) -> TreeNode:
-        n_samples, n_feats = len(X.columns.values)
-        
+    def _grow_tree(self, X: pd.DataFrame, y: pd.Series, depth:int = 0) -> TreeNode:
+        n_samples, n_feats = len(X.index), len(X.columns.values)
+        n_labels = len(y.unique())
 
         # Check the stopping criteria
         if (depth >= self.max_depth or n_labels == 1 or n_samples <self.min_samples_split):
             leaf_value = self._most_common_label(y)
             return TreeNode(value = leaf_value)
         
-        feat_idxs = np.random.choice(n_feats, self.n_feats, replace = False)
+        feat_idxs = np.random.choice(n_feats, len(self.numeric_features), replace = False)
 
-        best_feat_idx, best_thresh,  = _self_best_split(X, y, feat_idxs)
+        best_feat, best_thresh,  = self._best_split(X, y, feat_idxs)
+        print(best_feat)
+        print(best_thresh)
+        X_col = X[best_feat]
 
-        left_idx, right_idx = self._split(X[best_feat_idx], best_thresh)
-        
-        return TreeNode(best_feat_idx, best_thresh, left_idx, right_idx)
+        left_idx, right_idx = self._split(X_col, best_thresh)
 
-    def _best_split(self, X: np.DataFrame, y: np.Series, feat_idxs: list[int]):
+        left = self._grow_tree(X.loc[left_idx], y.loc[left_idx], depth+1)
+        right = self._grow_tree(X.loc[right_idx], y.loc[right_idx], depth+1)
+
+        return TreeNode(best_feat, best_thresh, left, right)
+    
+    def _most_common_label(self, y:pd.Series):
+        return y.value_counts().idxmax()
+
+    def _best_split(self, X: pd.DataFrame, y: pd.Series, feat_idxs: list[int]):
         best_gain = -1
         split_idx, split_thresh = None, None
 
         for feat_idx in feat_idxs:
-            X_col = X[self.features[feat_idx]]
+            X_col = X[feat_idx]
             thresholds = X_col.unique()
-
+            print("Thresh is,", thresholds)
             for thr in thresholds:
                 # Calculate 
-                gain = _self_information_gain(y, X_column, thr)
-
+                gain = self._information_gain(y, X_col, thr)
+                print("The gain is,", gain)
                 if gain > best_gain:
                     best_gain = gain
                     split_idx = feat_idx
-                    split_threshold = thr
-        return split_idx, split_threshold
+                    split_thresh = thr
+
+        return split_idx, split_thresh
     
-    def _information_gain(y: pd.Series, X_col: pd.Series, thr):
+    def _information_gain(self, y: pd.Series, X_col: pd.Series, thr):
         # Parent entropy
-        counts = y.value_counts()
-        parent_entropy = self._entropy(y)
+        counts = y.value_counts().values
+        parent_entropy = self._entropy(counts)
         
         # Create children
         left_idxs, right_idxs = self._split(X_col, thr)
 
-        if len(left_idxs) or len(right_idxs):
+        if len(left_idxs) == 0 or len(right_idxs) == 0:
             return 0
         
         # Calculate the wheighted gain
         n = len(y)
         n_l, n_r = len(left_idxs), len(right_idxs)
-        e_l, e_r = self._entropy(n_l), self._entropy(n_r)
+        e_l, e_r = self._entropy(y.loc[left_idxs]), self._entropy(y.loc[right_idxs])
         
         child_entropy =(n_l/n)*e_l + (r_l/n)*e_r
         
@@ -117,13 +132,13 @@ class DecisionTree:
 
         return information_gain
 
-    def _split(self, X_col: pd.Series, split_thr) -> tuple[list[int], list[int]]:
-        # This must be changed
-        left_idxs   = X_col[X_col <= split_thr]
-        right_idxs  = X_col[X_col > split_thr]
+    def _split(self, X_col: pd.Series, split_thr: int) -> tuple[list[int], list[int]]:
+        
+        left_idxs   = X_col[X_col <= split_thr].index
+        right_idxs  = X_col[X_col > split_thr].index
         return left_idxs, right_idxs
 
-    def _entropy(counts: np.array):
+    def _entropy(self, counts: np.array):
         """
         Computes the entropy of a partitioning
 
@@ -150,7 +165,7 @@ class DecisionTree:
     
     def _traverse_tree(self, x, node):
         if node.is_leaf_node():
-            return node.value()
+            return node.value
 
         if x[node.feature] <= node.threshold:
             return self._traverse_tree(x, node.left)
