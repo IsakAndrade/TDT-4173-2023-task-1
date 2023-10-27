c1006721431ec96faee03c4452e5f5938b06f8ca
= <=>
2023-10-27 19:18:27 +0200

    FEAT: Before divergence. Status: decision tree sort of working. Added bagging :/

--- decision_tree/decision_tree.py
+++ decision_tree/decision_tree.py
@@ -26,12 +26,13 @@ class TreeNode:
 
 class DecisionTree:
     
-    def __init__(self, min_samples_split: int = 2, max_depth: int = 100, n_features: list[str] = None):
+    def __init__(self, min_samples_split: int = 2, max_depth: int = 100, bagging: bool = False, bag_frac = 1.0, n_trees: int = 1):
         self.min_samples_split  = min_samples_split
         self.max_depth          = max_depth
-        self.n_features         = n_features
-        self.root               = None
-        self.rules              = []
+        self.bagging            = bagging
+        self.bag_frac           = bag_frac
+        self.rules              = [[] for i in range(n_trees)]
+        self.n_trees            = n_trees
     
     def fit(self, X: pd.DataFrame, y: pd.Series):
         """
@@ -44,54 +45,35 @@ class DecisionTree:
             y (pd.Series): a vector of discrete ground-truth labels
         """
 
-        # # Transforming the data so that it becomes easier to work with.
+        # Adding a bagging stage aka splitting data even further
+        if self.bagging:
+            for i in range(self.n_trees):
+                X_sample = X.sample(frac=self.bag_frac)
+                self._grow_tree(X = X_sample, y = y, rule_set= i)
+        else:
+            self._grow_tree(X = X, y = y, rule_set= 0)
+
+    def _grow_tree(self, X: pd.DataFrame, y: pd.Series, depth:int = 0, rule = [], rule_set: int = 1):
+        n_samples  = len(X.index)
+        n_labels = len(y.unique())
         
-        
-        # self.features = X.columns.values
-        # self.numeric_features = list(range(len(X.columns.values)))
-
-        # self.feature_mapping = [dict(zip(self.features, self.numeric_features)) for num in self.numeric_features][0]
-
-        # X_numeric = X.copy()
-        # X_numeric.columns = self.numeric_features
-
-        # thresh_mappings = []
-        # for numeric_feature, feature in enumerate(self.features):
-        #     thresh = X[feature].unique() 
-        #     thresh_numeric = list(range(len(thresh)))
-        #     thresh_mapping = [dict(zip(thresh, thresh_numeric)) for num in thresh_numeric][0]
-            
-            
-        #     data = X[feature]
-        #     X_numeric[numeric_feature] = pd.Series([thresh_mapping[val] for val in data])
-        #     thresh_mappings.append(thresh_mapping)
-        
-        # self.thresh_mappings = thresh_mappings
-        
-        # results = y.unique()
-        # numeric_results = list(range(len(results)))
-
-        # self.result_mapping = [dict(zip(results, numeric_results)) for res in numeric_results][0]
-
-        # self.rules = []
-        self.root = self._grow_tree(X, y)
-        # for rule in self.rules:
-        #     print(rule)
-
-    def _grow_tree(self, X: pd.DataFrame, y: pd.Series, depth:int = 0, rule = [], used_splits = []) -> TreeNode:
-        
+        if (depth >= self.max_depth or n_labels == 1 or n_samples < self.min_samples_split):
+                leaf_value = self._most_common_label(y)
+                self.rules[rule_set].append([rule] + [leaf_value])
+                
+                return 0
         feat_idxs = X.columns.values.copy()
         
         np.random.shuffle(feat_idxs)
 
         # Modify best split to take a set of blocked split.
-        best_feat = self._best_split(X.copy(), y.copy(), feat_idxs, used_splits)
+        best_feat = self._best_split(X.copy(), y.copy(), feat_idxs)
         
         best_threshs = X.copy()[best_feat].unique()
         
+        # Add a threshhold so that bad rules don't get added :/
         
-        print("The best feature is", best_feat)
-        print("The best thresh", best_threshs)
+        
         for thr in best_threshs: 
             
             X_col = X[best_feat]
@@ -101,22 +83,13 @@ class DecisionTree:
             # Check the stopping criteria
             y_new = y.copy().loc[new_idxs]
             X_new = X.copy().loc[new_idxs]
-
-            n_labels  = len(y_new.unique())
-            n_samples = len(X_new.index)
             
-            if (depth >= self.max_depth or n_labels == 1 or n_samples < self.min_samples_split):
-                leaf_value = self._most_common_label(y)
-                self.rules.append([new_rule] + [leaf_value])
-                print(self.rules)
+            n_samples_new = len(new_idxs) 
 
-                return 0
-            else:
-                X_new = X_new.drop(labels= best_feat, axis = 1)
-                # print(X_new.head())
-                print(new_rule)
-                self._grow_tree(X_new, y_new, depth+1, new_rule, used_splits)
-            
+        
+            X_new = X_new.drop(labels= best_feat, axis = 1)
+            self._grow_tree(X = X_new, y = y_new, depth = depth+1, rule = new_rule, rule_set =  rule_set)
+                
 
 
         return 0
@@ -124,47 +97,41 @@ class DecisionTree:
     def _most_common_label(self, y:pd.Series):
         return y.value_counts().idxmax()
 
-    def _best_split(self, X: pd.DataFrame, y: pd.Series, feat_idxs: list[int], used_splits):
-        best_gain = -1
+    def _best_split(self, X: pd.DataFrame, y: pd.Series, feat_idxs: list[int]):
+        best_gain = 10000
         split_idx, split_thresh = None, None
-        gain = 0
+        
+        
         for feat_idx in feat_idxs:
             X_col = X[feat_idx]
-            thresholds = X_col.unique()
             
-            for thr in thresholds:
-                # Calculate 
-                gain = gain + self._information_gain(y, X_col, thr)
-                
-            if gain > best_gain:
+            gain = self._total_entropy(y, X_col)
+            
+            
+            if gain < best_gain:
                 best_gain = gain
                 split_idx = feat_idx  
-            gain = -1
+        
         
         return split_idx
     
-    def _information_gain(self, y: pd.Series, X_col: pd.Series, thr):
-        # Parent entropy
-        counts = y.value_counts().values
-        parent_entropy = self._entropy(counts)
-        
-        # Create children
-        left_idxs, right_idxs = self._split(X_col, thr)
-
-        if len(left_idxs) == 0 or len(right_idxs) == 0:
-            return 0
-        
-        # Calculate the wheighted gain
+    def _total_entropy(self, y: pd.Series, X_col: pd.Series):
+        total_ent = 0
         n = len(y)
-        n_l, n_r = len(left_idxs), len(right_idxs)
-        e_l, e_r = self._entropy(y.loc[left_idxs].value_counts().values), self._entropy(y.loc[right_idxs].value_counts().values)
         
-        child_entropy =(n_l/n)*e_l + (n_r/n)*e_r
+        grouped  = X_col.groupby(X_col)
         
-        # Calculate Information Gain
-        information_gain  = parent_entropy - child_entropy
+        for name, group in grouped:
 
-        return information_gain
+            weight = len(group.index)/n
+            counts = y[group.index].value_counts().values
+
+            ent = self._entropy(counts)
+            
+            total_ent = total_ent + weight*ent
+
+
+        return total_ent
 
     def _split(self, X_col: pd.Series, split_thr: int) -> tuple[list[int], list[int]]:
         
@@ -213,127 +180,59 @@ class DecisionTree:
             ...
         ]
         """
-        # numeric_rules = self.inorderTraversal(self.root)
-        # print(numeric_rules)
-        # numeric_rules = []
-        # self._paths_to_leaves(self.root, [], numeric_rules)
-        # print(numeric_rules)
 
-        # rules = []
-
-        # for numeric_rule in numeric_rules:
-        #     num_feats_and_threshs = numeric_rule[0]
-            
-        #     feat_and_thresh = []
-
-        #     for num_feat_and_thresh in num_feats_and_threshs:
-        #         num_feat = num_feat_and_thresh[0]
-        #         num_thresh = num_feat_and_thresh[1]
-
-                
-                
-        #         feat = self._feat_numeric_to_string(num_feat)
-        #         thresh = self._thresh_numeric_to_string(num_feat, num_thresh)
-
-        #         feat_and_thresh.append((feat,thresh))
-
-            
-        #     result = numeric_rule[1]
-
-        #     rules.append((feat_and_thresh, result))
-
-        return self.rules
-    
-    def _feat_numeric_to_string(self, num_feat: int)->str:
-        
-        string_to_numeric_dic = self.feature_mapping
-        
-        numeric_to_string_dic = {v: k for k, v in string_to_numeric_dic.items()}
-        
-        feat = numeric_to_string_dic[num_feat]
-
-        return feat
-
-    def _thresh_numeric_to_string(self, num_feat: int, num_thresh: int)->str:
-        string_to_numeric_dic = self.thresh_mappings[num_feat]
-
-        numeric_to_string_dic = {v: k for k, v in string_to_numeric_dic.items()}
-        
-        thresh = numeric_to_string_dic[num_thresh]
-
-        return thresh
-    
-    def _res_numeric_to_string(self, num_res: int)->str:
-        string_to_numeric_dic = self.result_mapping
-
-        numeric_to_string_dic = {v: k for k, v in string_to_numeric_dic.items()}
-        
-        res = numeric_to_string_dic[num_res]
-
-        return res
-
-    def inorderTraversal(self, root):
-        res = []
-        if root:
-            res.append((root.feature, root.threshold))
-            if root.is_leaf_node():
-                res.append(root.value)
-            self.inorderTraversal(root.left)
-            self.inorderTraversal(root.right)
-        return res
-
-    def _paths_to_leaves(self, node, current_path, all_paths):
-
-        if node is None:
-            current_path = []
-
-        # Add the current node to the path
-        if node is not None:
-            current_path.append((node.feature, node.threshold, node.depth))
-            if node.is_leaf_node():
-                all_paths.append((current_path[:-1], node.value))
-            else:
-                # Recursive call for left and right subtrees
-                self._paths_to_leaves(node.left, current_path, all_paths)
-                self._paths_to_leaves(node.right, current_path, all_paths)
-
-            # Backtrack: Remove the current node from the current path
-            current_path.pop()
-
+        return self.rules[0]
     
     def predict(self, X: pd.DataFrame):
-        X_numeric = X.copy()
         
-        features = X_numeric.columns.values
-        X_numeric.columns = list(range(len(X_numeric.columns.values)))
+        
+        
+        #return  np.array([self._traverse_tree(X_numeric.loc[idx], self.root) for idx in X_numeric.index.tolist()])
+        
+        if self.bagging:
+            
+            y_list = [None for _ in list(range(self.n_trees))]
 
-        thresh_mappings = []
-        for numeric_feature, feature in enumerate(features):
-            thresh = X_numeric[numeric_feature].unique() 
-            thresh_numeric = list(range(len(thresh)))
-            thresh_mapping = [dict(zip(thresh, thresh_numeric)) for num in thresh_numeric][0]
-            thresh_mappings.append(thresh_mapping)
-            
-            data = X_numeric[numeric_feature].values
-            
-            
-            X_numeric[numeric_feature] = pd.Series([thresh_mapping[val] for val in data], index = X_numeric[numeric_feature].index)
-            
-        results = []
-        
-        
-        return  np.array([self._traverse_tree(X_numeric.loc[idx], self.root) for idx in X_numeric.index.tolist()])
+            for i, rule_set in enumerate(self.rules):
+                print(i)
+                y_list[i] = self._predict_with_rules(X.copy(), rule_set)
 
+            y_bagged = pd.concat(y_list, axis=1)
+            print(y_bagged.head())
+            row_modes = y_bagged.mode(axis=1)
+            
+            y = row_modes[0]
+            
+
+        else:
+            rules = self.rules[0]
+            y = self._predict_with_rules(X, rules)
+
+        return y
     
-    def _traverse_tree(self, x, node):
-        if node.is_leaf_node():
-            return node.value
+    def _predict_with_rules(self, X: pd.DataFrame, rules)-> pd.Series:
         
-        if x[node.feature] <= node.threshold:
-            return self._traverse_tree(x, node.left)
+        y = pd.Series(index=X.copy().index, dtype = str)
+        print(rules)
+        for rule in rules:
+                conditions_unformated = rule[0]
+                print(conditions_unformated)
+                result= rule[1]
+                
+                conditions = X.index >= 0
 
-        return self._traverse_tree(x, node.right)
+                for i, cond in enumerate(conditions_unformated):
+                    feat = cond[0]
+                    thr = cond[1]
+                    
+                    conditions = conditions & (X[feat] == thr)
+                
+                idxs = X[conditions].index.to_list()
+                for idx in idxs:
+                    y.loc[idx] = result
+                    
         
+        return y
 
 
 
